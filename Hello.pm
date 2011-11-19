032ac0848590a22adfe3436f144511532a6e59dc
Eric Wolf <coyocanid@gmail.com>
2011-11-18 21:40:06 -0800

    fixed login bug. fixed loading new package. added hello pakage

---
+++ Hello.pm
@@ -0,0 +1,13 @@
+package GServ::Hello;
+
+use GServ::AppObj;
+
+use base GServ::AppObj;
+
+sub hello {
+    my( $self, $data, $acct ) = @_;
+    my $name = $acct ? $acct->get_handle() : '?';
+    return { r => "hello there $name" };
+}
+
+1;
