44de3398925cf7c58bf4374cd1bc93b52097adb8
Eric Wolf <coyocanid@gmail.com>
2012-01-19 18:23:27 -0800

    added Makefile.PL and MANIFEST

---
+++ lib/Yote.pm
@@ -0,0 +1,45 @@
+pakcage Yote;
+
+use vars qw($VERSION);
+$Version = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Yote - a placeholder class for the packages below Yote.
+
+=head1 SYNOPSIS
+
+
+use Yote::WebAppServer;
+
+my $server = new Yote::WebAppServer();
+
+$server->start_server( port =E<gt> 8008,
+
+=over 32
+
+		       datastore => 'Yote::MysqlIO',
+		       db => 'yote_db',
+		       uname => 'yote_db_user',
+		       pword => 'yote_db-password' );
+
+=back
+
+or
+
+use Yote::WebAppServer;
+
+$server->start_server( port =E<gt> 8008,
+
+=over 32
+
+		       datastore => 'Yote::SQLiteIO',
+		       db => 'yote_db' );
+
+=back
+
+=cut
