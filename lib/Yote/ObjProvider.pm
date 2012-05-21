a5c41d7b6e1ab066aff79318542a7f31b1056774
Stephan Szabo <se@zombiestormtrooper.com>
2012-05-20 17:48:50 -0700

    ObjProvider: Fix bug where a new yote root was created each time. WebAppServer: Fix bug where anything with an underscore were rejected rather than only ones that start with an underscore. yote.js: Fix bug where arguments in _create_obj's inner function didn't line up with the addition of 'an' (which was removed).

--- lib/Yote/ObjProvider.pm
+++ lib/Yote/ObjProvider.pm
@@ -37,7 +37,7 @@ sub init {
     print STDERR Data::Dumper->Dump([$args]);
     $DATASTORE = new Yote::SQLiteIO( $args );
     $DATASTORE->ensure_datastore();
-    new Yote::YoteRoot(); #ensure that there is the singleton root object.
+    fetch(1) || new Yote::YoteRoot(); #ensure that there is the singleton root object.
 } #init
 
 sub disconnect {
