1d48304d839a655c5a9e0d6770e2316fd1d398be
Eric Wolf <coyocanid@gmail.com>
2015-05-26 14:18:15 -0700

    added stringify override for Yote::Obj

--- node/FixedStore.js
+++ node/FixedStore.js
@@ -1,11 +1,20 @@
 
 var fs = require('fs');
 
+
+
 module.exports = {
-    open: function( path, template, size, cb ) {
-
-        // template is an array of types
-
+    openSync: function( path, size, cb ) {
+        //check if this file exists
+        try {
+            fs.statSync( path );
+        } catch( err ) {
+            fs.appendFileSync( path, '' );
+        }
+        var fd = fs.openSync( path, 'r+' );
+        return create_store( fd, path, size );
+    },
+    open: function( path, size, cb ) {
         //check if this file exists
         try {
             fs.statSync( path );
@@ -14,70 +23,89 @@ module.exports = {
         }
 
         fs.open( path, 'r+', function( err, fd ) {
-            if( err ) {
-                return cb( err );
-            }
-            
-            var store = {
-                /*
-                  unlink: function() {},
-                  size:   function() { return size; },
-                  push:   function(record) { //adds record to the end of the store
-                  
-                  },
-                  empty:  function() {},
-                  ensure_entry_count : function( coune ) {
-                  },
-                  next_id: function() {
-                  
-                  },
-                */
-
-                getRecord: function( index ) {
-                    
-                },
-
-                putRecord: function( index, data ) {
-                    
-                },
-
-                numberOfEntries: function() {
-                    
-                },
-
-                nextId: function() {
-                    var next_id = this.numberOfEntries() + 1;
-                    this.put_record( next_id, [] );
-                    return next_id;
-                },
-
-
-                getRecordSync: function( index ) {
-                    var b = new Buffer(size);
-                    b.length = fs.readSync( fd, b, 0, size, size*index );
-                    return JSON.parse( b.toString() );
-                },
-
-                putRecordSync: function( index, data ) {
-                    var d = JSON.stringify(data);
-                    fs.writeSync( fd, new Buffer( d ), 0, d.length, size*index ) );
-                },
-
-                numberOfEntriesSync: function() {
-                    return Number.parseInt( fs.statSync( path ).size / size );
-                },
-
-                nextIdSync: function() {
-                    var next_id = this.numberOfEntries() + 1;
-                    this.putRecordSync( next_id, [] );
-                    return next_id;
-                },
-
-
-
-
-            };
-            cb( null, store );
-        } ); //filesystem call/back
+            if( err ) return cb( err );
+            cb( null, create_store( fd, path, size ) );
+        } );
     }, //open
+};
+
+var create_store = function( fd, path, size ) {
+    return {
+        /*
+          unlink: function() {},
+          size:   function() { return size; },
+          push:   function(record) { //adds record to the end of the store
+          
+          },
+          empty:  function() {},
+          ensure_entry_count : function( coune ) {
+          },
+          next_id: function() {
+          
+          },
+        */
+
+        getRecord: function( index, buffer ) {
+            var getSize = buffer.length > size ? size : buffer.length;
+            fs.read( fd, buffer, 0, getSize, (index-1)*size, function(err,bytesRead,buffer) {
+                if( err ) return cb( err );
+                cb( null, bytesRead, buffer );
+            } );
+        },
+
+        putRecord: function( index, buffer, cb ) {
+            var putSize = buffer.length > size ? size : buffer.length;
+            fs.write( fd, buffer, 0, putSize, (index-1)*size, function(err,byteswrote,buffer) {
+                if( err ) return cb( err );
+                cb( null, bytesWrote, buffer );
+            } );
+        },
+
+        numberOfEntries: function( cb ) {
+            fs.stat( path, function( err, stat ) {
+                if( err ) return cb( err );
+                cb( null, stat.size / size );
+            } );
+        },
+
+        nextId: function( cb ) {
+            Process.nextTick( function() { 
+                // the assignment of ids is necessarily a synchronous action
+                try {
+                    var next_id =this.nextIdSync();
+                } catch( err ) {
+                    cb( err );
+                }
+                cb( null, next_id );
+            } );
+        },
+
+
+        getRecordSync: function( index, buffer ) {
+            if( ! buffer ) buffer = new Buffer(size);
+            fs.readSync( fd, buffer, 0, size, size*(index-1) );
+            buffer.length = buffer.toString().indexOf( '\u0000' );
+            return buffer;
+        },
+
+        putRecordSync: function( index, buffer ) {
+            var putSize = buffer.length > size ? size : buffer.length;
+var wrote =  fs.writeSync( fd, buffer, 0, putSize, size*(index-1) );
+console.log( [ 'put',index, size*(index-1),buffer.toString(), putSize, buffer.length, wrote ] );
+            return wrote;
+        },
+
+        numberOfEntriesSync: function() {
+            return Number.parseInt( fs.statSync( path ).size / size );
+        },
+
+        nextIdSync: function() {
+            var next_id;
+            // the following line must be a single statement in order to be sure of correct execution
+            (next_id = ( 1 + Number.parseInt( fs.statSync( path ).size / size )) ) && this.putRecordSync( next_id, new Buffer( size ) );
+
+            return next_id;
+        }
+        
+    };
 };
--- node/test.js
+++ node/test.js
@@ -1,4 +1,27 @@
 var assert = require( 'assert' );
+var fs = require( 'fs' );
+
+console.log( '--- test fixed store---' );
+
+var stores = require( './FixedStore.js' );
+
+var tmpstore = '/tmp/foostore';
+var store = stores.openSync( tmpstore, 50 );
+assert.equal( store.nextIdSync(), 1, "first id" );
+assert.equal( fs.statSync( '/tmp/foostore' ).size, 50, "first chunk written" );
+assert.equal( store.nextIdSync(), 2, "second id" );
+assert.equal( store.nextIdSync(), 3, "third id" );
+assert.equal( fs.statSync( '/tmp/foostore' ).size, 150, "second and third chunks written" );
+store.putRecordSync( 2, "DROOPY" );
+store.putRecordSync( 5, "NUUUUU" );
+
+console.log( [ "T", store.getRecordSync( 1 ).toString(), store.getRecordSync( 2 ).toString() ] );
+assert.equal( store.getRecordSync( 2 ), "DROOPY", "wrote second record" );
+assert.equal( fs.statSync( '/tmp/foostore' ).size, 100, "second chunk written didnt change size" );
+
+fs.truncateSync( tmpstore );
+
+console.log( '--- yote store---' );
 
 var yote = require( './Yote.js' );
 var root = yote.getRoot();
