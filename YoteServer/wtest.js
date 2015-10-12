7de00697b5ab20622df618a55b9e4a4c0c411fc4
Eric Wolf <coyocanid@gmail.com>
2015-10-12 09:42:58 -0700

    updated license, readyme and changelog, and added warnings to DB::DataStore

--- YoteServer/wtest.js
+++ YoteServer/wtest.js
@@ -1,9 +1,8 @@
 importScripts( 'yote.js' );
 var root = yote.fetch_root(); // <--- got to here, but init is not fully complete
-console.log( [ root, root.data, root.cls, root.test, 'GOTTY ROO' ] );
-console.log( root.test( [ "WOO", 'AH' ] ) );
+
 
 onmessage = function(e) {
     console.log( "Worker got messsage from main page", e );
-    
+    var res = root.test( [ "WOO", 'AH' ] );
 }
