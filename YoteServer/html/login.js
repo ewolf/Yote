7b516d76cb138b2ebaff4e6bfd39c0162df60c1c
Eric Wolf <coyocanid@gmail.com>
2015-10-12 09:42:58 -0700

    updated license, readyme and changelog, and added warnings to DB::DataStore

--- YoteServer/html/login.js
+++ YoteServer/html/login.js
@@ -2,7 +2,7 @@ importScripts( '/__/js/yote.js' );
 yote.init();
 
 var root = yote.fetch_root();
-var app  = yote.fetch_app( 'myapp' );
+var app  = yote.fetch_app( 'Yote::App' );
 onmessage = function(e) {
     var data = e.data;
     var name = data[0], pw = data[1];
---
+++ YoteServer/lib/Yote/App.pm
@@ -0,0 +1,20 @@
+package Yote::App;
+
+use strict;
+use warnings;
+no warnings 'uninitialized';
+
+use 'Yote::Server';
+
+use base 'Yote::ServerApp';
+
+sub login {
+    my( $self, $un, $pw ) = @_;
+
+    return "Tried to log in with '$un' and '$pw'";
+
+} #login
+
+1;
+
+__END__
