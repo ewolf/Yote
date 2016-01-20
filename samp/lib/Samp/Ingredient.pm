cc9637fe0bc561992f11e10ba8494eeeba5b6ae2
Eric Wolf <coyocanid@gmail.com>
2016-01-20 10:20:59 -0800

    big change in parameter passing with this update. hashes and lists can be passed into yote without needing to be yote objects. conversely, a future update will change it so the reverse is also true

---
+++ samp/lib/Samp/Ingredient.pm
@@ -0,0 +1,23 @@
+package Samp::Ingredient;
+
+
+use strict;
+use warnings;
+no warnings 'uninitialized';
+
+use base 'Yote::Server::ListContainer';
+
+sub _allowedUpdates {
+    [ qw( product
+          notes
+          amount_per_run
+       ) ]
+}
+
+sub _when_added {
+    my( $self, $toProduct, $listName, $itemArgs ) = @_;
+    $self->set_product_line( $itemArgs );
+}
+
+
+1;
