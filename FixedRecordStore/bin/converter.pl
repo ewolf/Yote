882cbab6a5f728a0f81d34fda612e36b734733af
eric wolf <coyocanid@gmail.com>
2016-12-19 12:46:57 -0800

    added get_id method for fixedrec ordstore, added a compress database for yote.

--- FixedRecordStore/bin/converter.pl
+++ FixedRecordStore/bin/converter.pl
@@ -9,4 +9,4 @@ use Data::RecordStore;
 die "Converter requires Data::RecordStore version of at least 2.0" unless $Data::RecordStore::VERSION >= 2;
 
 my( $source_dir, $dest_dir ) = @ARGV;
-Data::RecordStore::( $source_dir, $dest_dir );
+Data::RecordStore::convert( $source_dir, $dest_dir );
