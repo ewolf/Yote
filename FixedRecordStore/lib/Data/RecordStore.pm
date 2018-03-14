a9e4ba1cd760b332ee4dc6e7ba554c9a4a537c59
Eric Wolf <coyocanid@gmail.com>
2018-03-14 14:47:28 -0700

    removed Make::Path

--- FixedRecordStore/lib/Data/RecordStore.pm
+++ FixedRecordStore/lib/Data/RecordStore.pm
@@ -126,7 +126,7 @@ use Data::Dumper;
 
 use vars qw($VERSION);
 
-$VERSION = '3.13';
+$VERSION = '3.15';
 
 use constant {
     DIRECTORY    => 0,
@@ -1366,6 +1366,6 @@ __END__
        under the same terms as Perl itself.
 
 =head1 VERSION
-       Version 3.13  (Dec 5, 2017))
+       Version 3.15  (Mar 14, 2017))
 
 =cut
